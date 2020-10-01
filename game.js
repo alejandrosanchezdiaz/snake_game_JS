febdc8de84c89aea9e6823469b386d4c3caf914b
Alex <alesandi.uk@gmail.com>
2020-10-01 16:25:52 +0100

    V1.1
    Created game.js and snake.js

---
+++ game.js
@@ -0,0 +1,47 @@
+    // Set up the game loop. The main function is gonna loop over and over again.
+    // With lastRenderTime and SNAKE_SPEED we can control how fast our game is render.
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED} from './snake.js'
+
+let lastRenderTime = 0;
+    // We create our gameBoard variable
+const gameBoard = document.getElementById('game-board');
+
+
+function main(currentTime){
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+    console.log('Render');
+    lastRenderTime = currentTime;
+
+    update();
+    draw();
+
+    // This game is gonna be broken into two different steps
+    // We're gonna have an 'update loop' and this loop is gonna 
+    // update all of the logic for our game.
+    // And we're gonna have another loop called 'draw' or 'render'
+    // and this is what's actually going to draw everything on the screen
+    // based on the update loop.
+    // So the update loop is going to move the snake to the correct
+    // position but not actually draw it. It's going to update if we ate 
+    // the food or not so it's gonna make the snake longer or shorter.
+    // It's also going to determine if we lost the game because we ran into
+    // our own tail or ran into the edge of the screen and then in the 
+    // draw, what we're gonna do is take all of the logic from update and 
+    // say okay where is our snake now let's actually draw him in the 
+    // correct position, let's draw the food where it needs to go..
+    
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+    updateSanke();
+}
+
+    // In order to print everything, we pass in our gameBoard
+function draw() {
+    drawSnake(gameBoard);
+}
---
+++ snake.js
@@ -0,0 +1,41 @@
+//SNAKE_SPEED controls the speed of the snake. The lowest, the slower.
+export const SNAKE_SPEED = 2;
+
+// Let's create a variable 'snakeBody' and this is going to
+// be an array of XY positions because our snake essentially is just a bunch of
+// different segments and these segments all are in its particular XY position on
+// the grid. The very center of our grid is going to be at X = 11 and at Y = 11.
+
+const snakeBody = [{ x: 11, y: 11}]
+
+//Let's export this to functions so we can use them on the Game.js file
+//This two functions happens together every SNAKE_SPEED seconds.
+export function update() {
+
+}
+
+
+// Before we can start drawing our snake we need to figure out how we're going to
+// represent our snake and the brilliant thing about using a grid-based system is
+// that we can represent our snake as essentially an x and a y position.
+
+// What we can do is take our snakeBody and we want to loop through each one of
+// our different segments (is what we'll call essentially each piece of the
+// snake we want to loop through) and I want to draw this to the screen but of
+// course I need to draw it to our game board so we're gonna need our game board
+// passed into this draw function so we can actually add our snake to it.
+
+export function draw(gameBoard) {
+    snakeBody.forEach(segment => {
+        // This is going to be a single div and this is going to go inside of our game
+        // board at a particular XY coordinates and since we're using grid we can directly
+        // set that XY coordinate very easily.
+        const snakeElement = document.createElement('div')
+        snakeElement.style.gridRowStart = segment.x;
+        snakeElement.style.gridColumnStart = segment.y;
+        // We add the blue color to our snake
+        snakeElement.classList.add('snake');
+        gameBoard.appendChild(snakeElement);
+
+    })
+}
